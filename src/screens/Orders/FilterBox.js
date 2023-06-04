24a09cf7e872942089527f554f81eee48616f04a
SUJIN S <sujin@applab.qa>
2023-06-04 23:04:17 +0530

    filter changes

--- src/screens/Orders/FilterBox.js
+++ src/screens/Orders/FilterBox.js
@@ -1,27 +1,27 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
-const FilterBox = ({item, selected, onPress}) => {
+const FilterBox = ({ item, selected, onPress }) => {
     return (
         <TouchableOpacity
             onPress={onPress}
             style={{
                 marginRight: 10,
                 borderRadius: 5,
-                backgroundColor: selected === item?.name ? '#58D36E' : '#fff',
+                backgroundColor: selected === item?.value ? '#58D36E' : '#fff',
                 shadowOffset: { height: 1, width: 1 },
                 elevation: 1,
                 shadowOpacity: 0.2,
                 minHeight: 30,
                 alignItems: 'center',
                 justifyContent: 'center',
-                marginVertical:10,
+                marginVertical: 10,
             }}
         >
             <Text
                 style={{
                     fontFamily: 'Poppins-Regular',
-                    color: selected === item?.name ? '#fff' : '#23233C',
+                    color: selected === item?.value ? '#fff' : '#23233C',
                     fontSize: 10,
                     paddingHorizontal: 15
                 }}
